d9781fda90a361d49d49c87b35dc840c872ec7cb
IsaacScheff <isaac1996@comcast.net>
2024-10-02 23:33:25 -0400

    Player can now rotate

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
     dt = 0
-    Clock = pygame.time.Clock()
+    clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)    
         screen.fill((0, 0, 0))
         player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
