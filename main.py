b08626e8a7b107b9eec39569a8c0fab54b3fecdc
IsaacScheff <isaac1996@comcast.net>
2024-10-02 23:10:41 -0400

    Created player

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
+    dt = 0
+    Clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
